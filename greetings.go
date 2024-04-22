0dc8ea229987ef8be6df00a0a6baf62bb0971af9
Varun <varunge@gmail.com>
2024-04-22 17:58:17 +0530

    first commit

---
+++ greetings.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var points = []int{20, 90, 100, 70}
+
+func sayHello(n string) {
+	fmt.Println("hello", n)
+	fmt.Println(score)
+}
--- main.go
+++ main.go
@@ -1,26 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
-
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-	return initials[0], "_"
-}
+var score = 99.5
 
 func main() {
-	fn, ln := getInitials("Varun")
-	fmt.Println(fn, ln)
+
+	sayHello("Varun")
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
 }
