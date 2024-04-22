284a137a801958317c7bea9ef668026b7be29f81
Varun <varunge@gmail.com>
2024-04-22 18:26:44 +0530

    first commit

--- main.go
+++ main.go
@@ -2,13 +2,40 @@ package main
 
 import "fmt"
 
-var score = 99.5
-
 func main() {
-
-	sayHello("Varun")
-
-	for _, v := range points {
-		fmt.Println(v)
+	//           key    value
+	menu := map[string]float64{
+		"soup":  4.99,
+		"pie":   7.99,
+		"salad": 3.55,
 	}
+
+	fmt.Println(menu)
+
+	fmt.Println(menu["pie"])
+
+	for k, v := range menu {
+		fmt.Printf("Key: %v, value: %0.2f\n", k, v)
+	}
+
+	// int as key type
+	phonebook := map[int]string{
+		234: "Book1",
+		235: "Book2",
+		236: "Book3",
+	}
+
+	fmt.Println(phonebook)
+
+	phonebook[234] = "Book_new"
+	fmt.Println(phonebook)
+
+	// map of map
+	map1 := map[int]map[int]string{
+		126328: phonebook,
+		// 343432: menu,
+	}
+
+	fmt.Println(map1)
+
 }
