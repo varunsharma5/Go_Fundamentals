26bf46cb84a8dcd6437d9bdf523bfbec9d83e5ec
Varun <varunge@gmail.com>
2024-04-23 16:07:48 +0530

    first commit

--- main.go
+++ main.go
@@ -2,40 +2,21 @@ package main
 
 import "fmt"
 
+func updateMenu(menu map[string]float64) {
+	menu["orange"] = 6.00
+}
+
 func main() {
-	//           key    value
+
+	// Group 2: map, slice, function
+
 	menu := map[string]float64{
-		"soup":  4.99,
-		"pie":   7.99,
-		"salad": 3.55,
+		"pie":   4.99,
+		"apple": 3.00,
 	}
+
+	updateMenu(menu)
 
 	fmt.Println(menu)
-
-	fmt.Println(menu["pie"])
-
-	for k, v := range menu {
-		fmt.Printf("Key: %v, value: %0.2f\n", k, v)
-	}
-
-	// int as key type
-	phonebook := map[int]string{
-		234: "Book1",
-		235: "Book2",
-		236: "Book3",
-	}
-
-	fmt.Println(phonebook)
-
-	phonebook[234] = "Book_new"
-	fmt.Println(phonebook)
-
-	// map of map
-	map1 := map[int]map[int]string{
-		126328: phonebook,
-		// 343432: menu,
-	}
-
-	fmt.Println(map1)
 
 }
