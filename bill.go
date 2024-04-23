013d4009973cc13e5c94154f7103f5288898e888
Varun <varunge@gmail.com>
2024-04-23 17:44:45 +0530

    first commit

--- bill.go
+++ bill.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// struct pointers are automatically dereferened
+// in the below receiver funcation, upon passing struct pointer, we only need to
+// chnage bill->*bill and no changes in required inside function as dereferencing is happing
+// automatically by Go compiler
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -13,7 +18,7 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{"pie": 3.99, "cake": 5.99},
+		items: map[string]float64{},
 		tip:   0,
 	}
 
@@ -21,7 +26,7 @@ func newBill(name string) bill {
 }
 
 // format the bill: Reciever function, binding this function with 'bill'
-func (b bill) format() string {
+func (b *bill) format() string {
 	fs := "Bill breakdown: \n"
 	var total float64 = 0
 
@@ -29,6 +34,17 @@ func (b bill) format() string {
 		fs += fmt.Sprintf("%v\t...$%v\n", k+":", v)
 		total += v
 	}
-	fs += fmt.Sprintf("%v\t...$%0.2f", "total:", total)
+
+	fs += fmt.Sprintf("%v\t...$%0.2f\n", "tip:", b.tip)
+
+	fs += fmt.Sprintf("%v\t...$%0.2f", "total:", total+b.tip)
 	return fs
+}
+
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -6,6 +6,9 @@ func main() {
 	var myBill bill
 	myBill = newBill("varun's bill")
 
+	myBill.addItem("cake", 5.99)
+	myBill.addItem("pie", 3.99)
+	myBill.updateTip(10)
 	fmt.Println(myBill.format())
 
 	// myBill.format()
